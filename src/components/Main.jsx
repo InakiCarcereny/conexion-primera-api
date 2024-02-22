906f53dabd9abe3ce623bc9faf2f31de81e14ed4
inakicarcereny <inakicarce@hotmail.com.ar>
2024-02-22 14:12:45 -0300

    agregando api de imagen

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,27 @@ import { useEffect, useState } from "react";
 
 const CAT_ENDPOINT = 'https://catfact.ninja/fact'
 
+const CAT_ENDPOINT_IMAGE = 'https://cataas.com/cat/says/ramon?fontSize=50&fontColor=red&json=true'
+
+const CAT_IMG_PREFIX = 'https://cataas.com'
+
+
 export function Main () {
 
-  const [catFact, setCatFact] = useState('')
+  const [catFact, setCatFact] = useState()
+  const [catImage, setCatImage] = useState()
 
   useEffect(() => {
     fetch (CAT_ENDPOINT)
     .then (res => res.json())
     .then (data => setCatFact(data.fact))
   }, [])
+
+  useEffect (() => {
+    fetch (CAT_ENDPOINT_IMAGE)
+    .then (res => res.json())
+    .then (data => setCatImage(data.url))
+  },[])
 
   //useEffect(() => {
   //  async function fetchCatFact() {
@@ -23,9 +35,10 @@ export function Main () {
   //}, []) 
 
   return (
-    <main className="flex flex-col gap-6">
+    <main className="flex flex-col justify-center items-center gap-6 mt-6 text-white font-bold text-3xl">
       <h1>Cat Facts</h1>
       <p>{catFact}</p>
+      <img src={`${CAT_IMG_PREFIX}${catImage}`} alt="" />
     </main>
   )
 }
